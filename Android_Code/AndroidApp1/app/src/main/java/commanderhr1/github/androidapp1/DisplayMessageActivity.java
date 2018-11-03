ab2ae8bb970f87c319d4351816a7d3af30250144
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2018-11-03 19:44:30 -0400

    Recompiled code, deisgned basic app.
    -Added another activity that displays inputted string
    -Added button and text boxes

---
+++ Android_Code/AndroidApp1/app/src/main/java/commanderhr1/github/androidapp1/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+//Displays EXTRA_MESSAGE after button is pushed.
+package commanderhr1.github.androidapp1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message_activity);
+
+        //Get the intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Capture the layout's TextView and set the string as it's text
+        TextView textView =findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
