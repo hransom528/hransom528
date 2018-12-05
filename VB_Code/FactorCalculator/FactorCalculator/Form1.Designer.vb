4c3e6861cda0a06bce45876f1ef48f55e5122190
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2018-12-05 17:50:20 -0500

    Added VB_Code and removed some

---
+++ VB_Code/FactorCalculator/FactorCalculator/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtNumberToFactor = New System.Windows.Forms.TextBox()
+        Me.txtFactor = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFileExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblTitle.Location = New System.Drawing.Point(112, 40)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(87, 13)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Factor Calculator"
+        '
+        'txtNumberToFactor
+        '
+        Me.txtNumberToFactor.Location = New System.Drawing.Point(25, 92)
+        Me.txtNumberToFactor.Name = "txtNumberToFactor"
+        Me.txtNumberToFactor.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumberToFactor.TabIndex = 1
+        '
+        'txtFactor
+        '
+        Me.txtFactor.Location = New System.Drawing.Point(179, 92)
+        Me.txtFactor.Name = "txtFactor"
+        Me.txtFactor.Size = New System.Drawing.Size(100, 20)
+        Me.txtFactor.TabIndex = 2
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblResult.Location = New System.Drawing.Point(112, 220)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(63, 13)
+        Me.lblResult.TabIndex = 3
+        Me.lblResult.Text = "Placeholder"
+        Me.lblResult.Visible = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.SystemColors.Control
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(346, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFileNew, Me.mnuFileExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'mnuFileNew
+        '
+        Me.mnuFileNew.Name = "mnuFileNew"
+        Me.mnuFileNew.Size = New System.Drawing.Size(180, 22)
+        Me.mnuFileNew.Text = "New "
+        '
+        'mnuFileExit
+        '
+        Me.mnuFileExit.Name = "mnuFileExit"
+        Me.mnuFileExit.Size = New System.Drawing.Size(180, 22)
+        Me.mnuFileExit.Text = "Exit"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.btnCalculate.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnCalculate.Location = New System.Drawing.Point(105, 144)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(88, 49)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "CALCULATE"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(346, 327)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtFactor)
+        Me.Controls.Add(Me.txtNumberToFactor)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents txtNumberToFactor As TextBox
+    Friend WithEvents txtFactor As TextBox
+    Friend WithEvents lblResult As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents mnuFileNew As ToolStripMenuItem
+    Friend WithEvents mnuFileExit As ToolStripMenuItem
+    Friend WithEvents btnCalculate As Button
+End Class
---
+++ VB_Code/FactorCalculator/FactorCalculator/Form1.vb
@@ -0,0 +1,38 @@
+﻿'Creator: Harris Ransom
+'Date: December 3rd, 2018
+
+Public Class Form1
+    'Declares variables 
+    Dim numToFactor As Double 'The number being factored
+    Dim factor As Double 'The factor being used in the calculation
+    Dim isFactor As Boolean 'True/False value determining if number is a factor or not
+
+    'Calculates the factor 
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        numToFactor = txtNumberToFactor.Text
+        factor = txtFactor.Text
+        lblResult.Visible = True
+
+        'Determines if number is factor, then displays result
+        If (numToFactor Mod factor = 0) Then
+            lblResult.Text = CStr(factor) + " is a factor of " + CStr(numToFactor)
+        Else
+            lblResult.Text = CStr(factor) + " is not a factor of " + CStr(numToFactor)
+        End If
+
+    End Sub
+    'Clears the program from the menu strip
+    Private Sub mnuFileNew_Click(sender As Object, e As EventArgs) Handles mnuFileNew.Click
+        lblResult.Text = ""
+        lblResult.Visible = False
+        txtFactor.Text = ""
+        txtNumberToFactor.Text = ""
+
+    End Sub
+
+    'Exits the program from the menu strip
+    Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
+        Application.Exit()
+    End Sub
+
+End Class
