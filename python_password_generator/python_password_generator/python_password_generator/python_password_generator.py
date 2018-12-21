bc1a75b99a2a3945d42a6c7b7de9b92a33f76043
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2018-12-21 14:26:07 -0500

    Created VS python_password_generator Project
    -Visual Studio
    -Language: Python

---
+++ python_password_generator/python_password_generator/python_password_generator/python_password_generator.py
@@ -0,0 +1,21 @@
+#!/bin/python
+import random;
+
+#Defines all possible characters for the password
+chars= "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()=+-~/<>"
+#Gets user input
+length=input("Password Length?");
+#Changes user input to an integer value
+length = int(length)
+#Defines the password variable 
+password = ''
+
+#Determines if the length is valid, then generates password, else, displays error
+if (length > 0):   
+    for c in range(length):
+     password += random.choice(chars)
+else:
+    print("Error! Invalid Length!")
+
+#Displays the password
+print (password)
