6fc33a82a4e7c7bbdc4ed356935c49333bc04652
Harris Ransom's RPI <hransom528@gmail.com>
2018-12-22 21:07:39 -0500

    Added python password generator

--- python_password_generator/python_password_generator/python_password_generator/python_password_generator.py
+++ python_password_generator/python_password_generator/python_password_generator/python_password_generator.py
@@ -1,21 +1,22 @@
 #!/bin/python
 import random;
+import sys;
 
 #Defines all possible characters for the password
-chars= "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()=+-~/<>"
+chars= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()=+-~/<>"
 #Gets user input
 length=input("Password Length?");
 #Changes user input to an integer value
 length = int(length)
 #Defines the password variable 
 password = ''
-
+    
 #Determines if the length is valid, then generates password, else, displays error
 if (length > 0):   
     for c in range(length):
      password += random.choice(chars)
 else:
-    print("Error! Invalid Length!")
+    print('Error! Invalid Length!')
 
 #Displays the password
-print (password)
+print ("PASSWORD: %s" % password)
