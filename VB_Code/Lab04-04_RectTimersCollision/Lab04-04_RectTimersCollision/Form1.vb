98cd257f14363b4a52ac7fd2947ae9e193e071c7
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-01-04 16:38:53 -0500

    Added Lab04-04_RectTimersCollision
    -Aka Pong
    -For school 1/4/19

---
+++ VB_Code/Lab04-04_RectTimersCollision/Lab04-04_RectTimersCollision/Form1.vb
@@ -0,0 +1,314 @@
+﻿'Harris Ransom
+'Class 8
+'Lab04-04_RectTimersCollision
+Imports System 'For RNG 
+
+Public Class Form1
+    'Initiates rectangles
+    Dim player1 As Rectangle 'Initiates player1 paddle
+    Dim player2 As Rectangle 'Initiates player2 paddle
+    Dim ball As Rectangle 'Initiates ball 
+    Dim topWall As Rectangle 'Initiates Top boundry
+    Dim bottomWall As Rectangle 'Initiates Bottom boundry
+    Dim leftWall As Rectangle 'Initates the left side boundry (left goal)
+    Dim rightWall As Rectangle 'Initiates the right side boundry (right goal)
+
+    'Sets up the player's paddles
+    Const PLAYER1X As Integer = 50 'X position of player 1
+    Dim player1Y As Integer = 225 'Y position of player 1
+    Const PLAYER2X As Integer = 700 'X position of player 2
+    Dim player2Y As Integer = 225 'Y position of player 2
+    Const P1YSTART As Integer = 225
+    Const P2YSTART As Integer = 225
+
+    'Sets up the ball
+    Dim ballX As Integer = 360 'Ball x position
+    Dim ballY As Integer = 225 'Ball y position
+    Const BALLSPEED = 20 'This is for resuming after pause
+    Dim ballMoveX As Integer = BALLSPEED 'Ball X movement speed
+    Dim ballMoveY As Integer = BALLSPEED 'Ball Y movement speed
+    Dim ballStartingX = 360 'X value of the ball at the start, for reseting it's position
+    Dim ballStartingY = 225 'Y value of the ball at the start, for reseting it's position
+
+    'Sets up the score system
+    Dim score1 As Integer = 0 'Player 1's score
+    Dim score2 As Integer = 0 'Player 2's score
+    Dim player1Score As Boolean = False 'Detects if ball was scored by player 1
+    Dim player2Score As Boolean = False 'Detects if ball was scored by player 2
+
+    'Sets up movement
+    Dim playerMoveUp As Integer = -15 'Pixels that each player moves up
+    Dim playerMoveDown As Integer = 15 'Pixels that each player moves down
+    Dim stopMove As Integer = 0 'Value to stop the player from moving
+    Dim player1KeyPressUp As Boolean = False 'Detects if player 1 is pressing up
+    Dim player1KeyPressDown As Boolean = False 'Detects if player 1 is pressing down 
+    Dim player2KeyPressUp As Boolean = False 'Detects if player 2 is pressing w
+    Dim player2KeyPressDown As Boolean = False 'Detects if player2 is pressing s
+
+    'Moves player1 (left paddle) up
+    Public Sub player1moveUp()
+        player1Y += playerMoveUp
+    End Sub
+
+    'Moves player1 (left paddle) down
+    Public Sub player1moveDown()
+        player1Y += playerMoveDown
+    End Sub
+
+    'Moves player2 (right paddle) up
+    Public Sub player2moveUp()
+        player2Y += playerMoveUp
+    End Sub
+
+    'Moves player2 (right paddle) down
+    Public Sub player2moveDown()
+        player2Y += playerMoveDown
+    End Sub
+
+    'Stops player 1
+    Public Sub stopMovementPlayer1()
+        player1Y += stopMove
+    End Sub
+
+    'Stops player 2
+    Public Sub stopMovementPlayer2()
+        player2Y += stopMove
+    End Sub
+
+    'Stops ball movement
+    Public Sub stopMovementBall()
+        ballMoveX = stopMove
+        ballMoveY = stopMove
+    End Sub
+
+    'Pause 
+    Public Sub pause()
+        stopMovementPlayer2()
+        stopMovementPlayer1()
+        stopMovementBall()
+        lblPause.Visible = True
+    End Sub
+
+    'Unpause
+    Public Sub unpause()
+        lblPause.Visible = False
+        ballMoveX = BALLSPEED
+        ballMoveY = BALLSPEED
+    End Sub
+
+    'Reset
+    Public Sub reset()
+        score1 = 0
+        score2 = 0
+        ballX = ballStartingX
+        ballY = ballStartingY
+        player1Y = P1YSTART
+        player2Y = P2YSTART
+    End Sub
+
+    'Handles collision
+    Public Sub collision()
+        'If player 1 hits the ball
+        If (player1.IntersectsWith(ball)) Then
+            ballMoveX = -ballMoveX
+            ballMoveY = -ballMoveY
+            ballX += ballMoveX
+            ballY += ballMoveY
+        End If
+
+        'If player 2 hits the ball
+        If (player2.IntersectsWith(ball)) Then
+            ballMoveX = -ballMoveX
+            ballMoveY = -ballMoveY
+            ballX += ballMoveX
+            ballY += ballMoveY
+        End If
+
+        'If ball hits top
+        If (ball.IntersectsWith(topWall)) Then
+            ballMoveY = -ballMoveY
+            ballY += ballMoveY
+        End If
+
+        'If ball hits bottom
+        If (ball.IntersectsWith(bottomWall)) Then
+            ballMoveY = -ballMoveY
+            ballY += ballMoveY
+        End If
+
+        'If player 1 hits a wall
+        If (player1.IntersectsWith(topWall)) Then
+            stopMovementPlayer1()
+        ElseIf (player1.IntersectsWith(bottomWall)) Then
+            stopMovementPlayer1()
+        End If
+
+        'If player 2 hits a wall
+        If (player2.IntersectsWith(topWall)) Then
+            stopMovementPlayer2()
+        ElseIf (player2.IntersectsWith(bottomWall)) Then
+            stopMovementPlayer2()
+        End If
+
+        'If player 2 scores
+        If (ball.IntersectsWith(leftWall)) Then
+            player2Score = True
+        End If
+
+        'If player 1 scores
+        If (ball.IntersectsWith(rightWall)) Then
+            player1Score = True
+        End If
+    End Sub
+
+
+    'Player 1 scores
+    Public Sub goal1()
+        score1 += 1
+        ballMoveX = -ballMoveX
+        ballX = ballStartingX
+        ballY = ballStartingY
+    End Sub
+
+    'Player 2 scores
+    Public Sub goal2()
+        score2 += 1
+        ballMoveX = -ballMoveX
+        ballX = ballStartingX
+        ballY = ballStartingY
+    End Sub
+
+    'Handles KeyDown events
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+        'Detects if player 2 is pressing a key
+        If (e.KeyCode = Keys.Up) Then
+            player2KeyPressUp = True
+        ElseIf (e.KeyCode = Keys.Down) Then
+            player2KeyPressDown = True
+        End If
+
+        'Detects if player 1 is pressing a key
+        If (e.KeyCode = Keys.W) Then
+            player1KeyPressUp = True
+        ElseIf (e.KeyCode = Keys.S) Then
+            player1KeyPressDown = True
+        End If
+
+        'Moves player 1
+        If (player1KeyPressDown = True) Then
+            player1moveDown()
+            collision()
+            Me.Refresh()
+        ElseIf (player1KeyPressUp = True) Then
+            player1moveUp()
+            collision()
+            Me.Refresh()
+        End If
+
+        'Moves player 2
+        If (player2KeyPressDown = True) Then
+            player2moveDown()
+            collision()
+            Me.Refresh()
+        ElseIf (player2KeyPressUp = True) Then
+            player2moveUp()
+            collision()
+            Me.Refresh()
+        End If
+    End Sub
+
+    'Paint event
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        'Draws player1 paddle
+        player1 = New Rectangle(PLAYER1X, player1Y, 25, 100)
+        e.Graphics.FillRectangle(Brushes.White, player1)
+
+        'Draws player2 paddle
+        player2 = New Rectangle(PLAYER2X, player2Y, 25, 100)
+        e.Graphics.FillRectangle(Brushes.White, player2)
+
+        'Draws ball
+        ball = New Rectangle(ballX, ballY, 12, 12)
+        e.Graphics.FillRectangle(Brushes.White, ball)
+
+        'Draws top boundry
+        topWall = New Rectangle(0, 132, 800, 15)
+        e.Graphics.FillRectangle(Brushes.White, topWall)
+        'Draws bottom boundry
+        bottomWall = New Rectangle(0, 450, 800, 30)
+        e.Graphics.FillRectangle(Brushes.White, bottomWall)
+        'Draws left boundry
+        leftWall = New Rectangle(0, 0, 30, 450)
+        'Draws right boundry
+        rightWall = New Rectangle(800, 0, 30, 450)
+    End Sub
+
+    'Detects if a player releases a key
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        'Player 1 code
+        If (e.KeyCode = Keys.W) Then
+            stopMovementPlayer1()
+            player1KeyPressUp = False
+            Me.Refresh()
+        ElseIf (e.KeyCode = Keys.S) Then
+            stopMovementPlayer1()
+            player1KeyPressDown = False
+            Me.Refresh()
+        End If
+
+        'Player 2 code
+        If (e.KeyCode = Keys.Up) Then
+            stopMovementPlayer2()
+            player2KeyPressUp = False
+            Me.Refresh()
+        ElseIf (e.KeyCode = Keys.Down) Then
+            stopMovementPlayer2()
+            player2KeyPressDown = False
+            Me.Refresh()
+        End If
+
+    End Sub
+
+    'KeyPress Event
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        'Pausing
+        If (e.KeyChar = "p") Then
+            pause()
+        ElseIf (e.KeyChar = "u") Then
+            unpause()
+        ElseIf (e.KeyChar = "r") Then
+            reset()
+        End If
+    End Sub
+
+    'Load event
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Starts timer
+        tmrTimer.Start()
+    End Sub
+
+    'Ticks every 100 ms
+    Private Sub tmrTimer_Tick(sender As Object, e As EventArgs) Handles tmrTimer.Tick
+        collision()
+        Me.Refresh()
+
+        'Moves ball
+        ballX += ballMoveX
+        ballY += ballMoveY
+
+        'Evaluates score
+        If (player1Score = True) Then
+            goal1()
+            player1Score = False
+        ElseIf (player2Score = True) Then
+            goal2()
+            player2Score = False
+        End If
+
+        'Displays score
+        lblScore1.Text = "Player 1 Score: " + FormatNumber(score1, 0)
+        lblScore2.Text = "Player 2 Score: " + FormatNumber(score2, 0)
+        Me.Refresh()
+    End Sub
+End Class
