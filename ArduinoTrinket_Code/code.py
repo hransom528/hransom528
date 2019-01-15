8dd703f983d78bae26932c8fbd3f0299520cd83d
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-01-14 20:50:29 -0500

    Added CircuitPython trinket m0 code

--- ArduinoTrinket_Code/code.py
+++ ArduinoTrinket_Code/code.py
@@ -1,13 +1,25 @@
 # Write your code here :-)
+#Imports libraries
 import board
 import digitalio
 import time
 
+#Sets up I/O pins
 led = digitalio.DigitalInOut(board.D13)
+output = digitalio.DigitalInOut(board.D4)
 led.direction = digitalio.Direction.OUTPUT
 
+#Connects to serial console in Mu
+print ("Connecting to the serial console...")
+time.sleep(0.775)
+print ("Connected!")
+
+#While loop runs forever
 while True:
     led.value = True
-    time.sleep(5.0)
+    output.value = True
+    time.sleep(1.0)
     led.value = False
-    time.sleep(5.0)
+    output.value = False
+    time.sleep(1.0)
+    #output = digitalio.Direction.OUTPUT
