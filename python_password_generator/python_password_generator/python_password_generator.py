72ced3b4a06291671b038c1ba5b746e311632e91
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-01-14 20:36:14 -0500

    Updated python_password_generator

--- python_password_generator/python_password_generator/python_password_generator.py
+++ python_password_generator/python_password_generator/python_password_generator.py
@@ -1,22 +1,70 @@
-#!/bin/python
-import random;
-import sys;
+#!/usr/bin/python
+import sys
+import secrets
 
 #Defines all possible characters for the password
-chars= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()=+-~/[]{};:<>`"
-#Gets user input
-length=input("Password Length?");
-#Changes user input to an integer value
-length = int(length)
-#Defines the password variable
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nums = "1234567890"
+special_char = "!@#$%^&*()=+-~/[]{};:<>`"
+
+#Defines variables
 password = ''
+count = 0
+length_error = False
+capital_error = False
+num_error = False
+special_error = False
+#Gets user input
+input("Press any button to continue...")
+length = input("Password Length?")
+capital_req = input("Capital letter requirement number?")
+num_req = input("Number requirement?")
+special_req = input("Special character requirement number?")
 
-#Determines if the length is valid, then generates password, else, displays error
-if (length > 0):
-    for c in range(length):
-     password += random.choice(chars)
+#Changes input to an integer value
+length = int(length)
+capital_req = int(capital_req)
+num_req = int(num_req)
+special_req = int(special_req)
+
+#Determines if properties are valid, then generates password, else, displays error
+    #Determines if there is an error
+if (length <= 0):
+    length_error = True
+if (capital_req < 0):
+    capital_error = True
+if (num_req < 0):
+    num_error = True
+if (special_req < 0):
+    special_error = True
+
+    #Displays error messages
+if (length_error == True):
+    print("Error! Invalid length!")
+if (capital_error == True):
+    print("Error! Invalid capital requirement!")
+if (num_error == True):
+    print("Error! Invalid number requirement!")
+if (special_error == True):
+    print("Error! Invalid special character requirement!")
+
+#Determines if there was an error. If there wasn't, then the password is generated
+if (length_error == False) and (capital_error == False) and (num_error == False) and (special_error == False):
+    while (count < length):
+        if (capital_req > 0) and (count < length):
+            password += secrets.choice(uppercase)
+            count += 1
+
+        if (num_req > 0) and (count < length):
+            password += secrets.choice(nums)
+            count += 1
+
+        if (special_req > 0) and (count < length):
+            password += secrets.choice(special_char)
+            count += 1
+    #Displays the password
+    print ("PASSWORD: %s" % password)
 else:
-    print('Error! Invalid Length!')
+    print("Please fix any errors.")
 
-#Displays the password
-print ("PASSWORD: %s" % password)
