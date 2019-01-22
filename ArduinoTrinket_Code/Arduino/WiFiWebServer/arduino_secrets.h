3c36b60163c18aeceb77ebacf91f002602595546
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-01-22 17:44:57 -0500

    Added C + C++ Hello World programs

---
+++ ArduinoTrinket_Code/Arduino/WiFiWebServer/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID " Meir"
+#define SECRET_PASS "Mhra1424"
---
+++ C_C++_Code/Hello_World.cpp
@@ -0,0 +1,8 @@
+//Hello World program
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;
+}
