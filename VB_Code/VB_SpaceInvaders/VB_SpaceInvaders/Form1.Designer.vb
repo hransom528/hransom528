d327fae6357ff0535bdd6f95dddd4c219b1eb24a
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-01-29 19:27:46 -0500

    Worked on Space Invaders

--- VB_Code/VB_SpaceInvaders/VB_SpaceInvaders/Form1.Designer.vb
+++ VB_Code/VB_SpaceInvaders/VB_SpaceInvaders/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,9 +20,12 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.tmrTimer = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'lblTitle
@@ -31,18 +34,34 @@ Partial Class Form1
         Me.lblTitle.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblTitle.ForeColor = System.Drawing.Color.Lime
-        Me.lblTitle.Location = New System.Drawing.Point(281, 9)
+        Me.lblTitle.Location = New System.Drawing.Point(238, 9)
         Me.lblTitle.Name = "lblTitle"
         Me.lblTitle.Size = New System.Drawing.Size(267, 31)
         Me.lblTitle.TabIndex = 0
         Me.lblTitle.Text = "SPACE INVADERS"
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.ForeColor = System.Drawing.Color.Lime
+        Me.lblScore.Location = New System.Drawing.Point(30, 19)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(86, 20)
+        Me.lblScore.TabIndex = 1
+        Me.lblScore.Text = "Score:  0"
+        '
+        'tmrTimer
+        '
+        Me.tmrTimer.Interval = 25
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(784, 461)
+        Me.Controls.Add(Me.lblScore)
         Me.Controls.Add(Me.lblTitle)
         Me.ForeColor = System.Drawing.SystemColors.ControlText
         Me.Name = "Form1"
@@ -53,4 +72,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents lblTitle As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents tmrTimer As Timer
 End Class
--- VB_Code/VB_SpaceInvaders/VB_SpaceInvaders/Form1.vb
+++ VB_Code/VB_SpaceInvaders/VB_SpaceInvaders/Form1.vb
@@ -3,9 +3,118 @@
 'Created 1/29/19
 
 Public Class Form1
-    Dim ship As PictureBox
-    Dim fort1 As PictureBox
-    Dim fort2 As PictureBox
-    Dim fort3 As PictureBox
-    Dim fort4 As PictureBox
+    ''Declaration Statements
+    'Player pictureboxes
+    Dim ship As PictureBox 'Player's ship
+    Dim fort1 As PictureBox 'Farthest fort to the left
+    Dim fort2 As PictureBox 'Fort to the right of fort 1
+    Dim fort3 As PictureBox 'Fort to the left of fort 4
+    Dim fort4 As PictureBox 'Farthest fort to the right
+    Dim life As PictureBox 'Life picture
+
+    'Lasers and Missles 
+    Dim alienLaser1 As Rectangle 'One laser on screen
+    Dim alienLaser2 As Rectangle 'Another laser on screen
+    Dim alienLaser3 As Rectangle 'Another ANOTHER laser on screen
+    Dim shipMissle As PictureBox 'Laser from ship
+
+    'Alien pictureboxes
+    Dim alien1 As PictureBox
+    Dim alien2 As PictureBox
+    Dim alien3 As PictureBox
+    Dim alien4 As PictureBox
+    Dim alien5 As PictureBox
+    Dim alien6 As PictureBox
+    Dim alien7 As PictureBox
+    Dim alien8 As PictureBox
+    Dim alien9 As PictureBox
+    Dim alien10 As PictureBox
+    Dim alien11 As PictureBox
+    Dim alien12 As PictureBox
+    Dim alien13 As PictureBox
+    Dim alien14 As PictureBox
+    Dim alien15 As PictureBox
+    Dim alien16 As PictureBox
+    Dim alien17 As PictureBox
+    Dim alien18 As PictureBox
+    Dim alien19 As PictureBox
+    Dim alien20 As PictureBox
+    Dim alien21 As PictureBox
+    Dim alien22 As PictureBox
+    Dim alien23 As PictureBox
+    Dim alien24 As PictureBox
+    Dim alien25 As PictureBox
+
+    'Misc.
+    Dim randomShip As Integer = 0 'Selects a random ship to shoot a laser from
+    Dim shipAmmount As Integer = 25 'Ammount of ships on screen
+    Dim score As Integer = 0 'Player score
+    Dim lives As Integer = 3 'Player lives, starts at 3
+    Const PLAYERMOVESPEED As Integer = 10 'Movement speed of the player
+
+#Region "Methods"
+    'Moves ship right
+    Public Sub playerMoveRight()
+
+    End Sub
+
+    'Moves ship left
+    Public Sub playerMoveLeft()
+
+    End Sub
+
+    'Moves aliens left
+    Public Sub alienMoveLeft()
+
+    End Sub
+
+    'Moves aliens right
+    Public Sub alienMoveRight()
+
+    End Sub
+
+    'Shoots a laser from a specific enemy
+    Public Sub alienShoot(alien, lasernum)
+
+    End Sub
+
+    'Shoots a missle from the player
+    Public Sub playerShoot()
+
+    End Sub
+
+    'Handles all of the collision
+    Public Sub collision()
+
+    End Sub
+#End Region
+
+#Region "Events"
+    'Paint Event
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+
+    End Sub
+
+    'Keydown Event
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+    End Sub
+
+    'Keyup event
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+
+    End Sub
+
+    'Keypress Event
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+
+    End Sub
+#End Region
+
+    'Timer that ticks every 25ms
+    Private Sub tmrTimer_Tick(sender As Object, e As EventArgs) Handles tmrTimer.Tick
+
+    End Sub
+
+
 End Class
