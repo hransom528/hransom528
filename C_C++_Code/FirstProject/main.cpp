57b09e81dd386bd31c0fad81b8a30cef416f7489
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-02-24 17:51:33 -0500

    Added test form, changed FirstProject

--- C_C++_Code/FirstProject/main.cpp
+++ C_C++_Code/FirstProject/main.cpp
@@ -4,10 +4,5 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << 2 + 2 << endl;
-    cout << true << endl;
-    int myNum = 2+2;
-    cout << myNum << endl;
-    return 0;
+
 }
