747d02e5cda31461a20c2777cb41a990abf8a2b1
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-02-25 20:20:48 -0500

    Modified Fortran test program

--- Fortran_Code/TestProgram/main.f95
+++ Fortran_Code/TestProgram/main.f95
@@ -1,1 +1,8 @@
-
+program main
+    implicit none
+    logical :: a
+    print *, "Test out the NOT operator by typing TRUE or FALSE!"
+    read *, a
+    a = .not.(a)
+    print *, "Result: ", a
+end program
