2ca268bed94bf2ba4e796a3a6bb7bd6b931bb6f2
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2019-04-12 21:00:44 -0400

    Random Stuff

--- Python_Code/determineOS.py
+++ Python_Code/determineOS.py
@@ -1,12 +1,11 @@
 import sys 
 
 if (sys.platform == 'win32'):
-	print('Hello, Windows User!')
+        print('Hello, Windows User!')
 elif (sys.platform == 'linux'):
 	print('Greetings, Linux Geek!')
 elif (sys.platform == 'darwin'):
 	print('Ew, a Mac User!')
-
 
 input('Press ENTER to close...')
 
