7d73cebd2e3fd00c6741e35939a82c98f6b34eb2
harris <hransom528@gmail.com>
2019-08-27 22:30:38 -0400

    Added stockAnalysis.py

--- Python_Code/stocker/stockAnalysis.py
+++ Python_Code/stocker/stockAnalysis.py
@@ -1,19 +1,11 @@
 #Analysis of stocks. Utilizes Stocker, as well as MatPlotLib/SciPy
 from stocker import Stocker
-import matplotlib
 
 #Initializes stocks
-dow = Stocker('DJI')
 microsoft = Stocker('MSFT')
 redhat = Stocker('RHT')
 
-microsoft_history = microsoft.stock()
-redhat_history = redhat.stock()
 
-
-#Retrieves stock data
-microsoft.head()
-redhat.head()
-microsoft.plot_stock()
+#Retrieves and plots stock data
+microsoft.plot_stock(start_date=None,end_date=None, stats=['Adj. Close'], plot_type='basic')
 redhat.plot_stock()
-
