e3ddd3b19dc3c72d31fe9ed0f0e230ff1a4acc12
harris <hransom528@gmail.com>
2020-01-12 18:43:50 -0500

    Updated CPP and Java workspaces for IDEs

---
+++ C_C++_Code/CodeBlocks Projects/nthPi/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double threshold;
+    double piVal = 0;
+    cout << "Threshold of decimal places for Pi (ex. 0.0003): " << endl();
+    cin >> threshold;
+
+    return 0;
+}
