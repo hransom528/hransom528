175ee15d0788a151b199951d2200ea0287e61c2a
commanderHR1 <39062223+commanderHR1@users.noreply.github.com>
2020-02-17 00:04:23 -0500

    Updated Grid class

--- JavaCode/JavaCode/src/Grid.java
+++ JavaCode/JavaCode/src/Grid.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 /**
  * @author Harris Ransom
  * @param <T>
  */
-public class Grid<T> implements Iterable<T>{
+public class Grid<T> implements Iterable<T>, Collection<T>{
 	private int xSize;
 	private int ySize;
 	private ArrayList<ArrayList<T>> grid;
@@ -14,6 +15,7 @@ public class Grid<T> implements Iterable<T>{
 	 * @param xSize
 	 * @param ySize
 	 */
+	@SuppressWarnings("unchecked")
 	public Grid (int xSize, int ySize) {
 		this.xSize = xSize;
 		this.ySize = ySize;
@@ -46,7 +48,113 @@ public class Grid<T> implements Iterable<T>{
 		return this.ySize;
 	}
 
-	/**Returns grid in printableString form
+	@Override
+	/**
+	 * @see java.util.Collection#size()
+	 */
+	public int size() {
+		if ((long) (this.getxSize() + this.getySize()) > Integer.MAX_VALUE) {
+			return Integer.MAX_VALUE;
+		}
+		else {
+			return (this.getxSize() * this.getySize());
+		}
+	}
+
+	/**Gets object at a specific coordinate
+	 * @param xCoord
+	 * @param yCoord
+	 * @return returnObject
+	 */
+	public Object getVal(int xCoord, int yCoord) {
+		Object returnObject = new Object();
+		try {
+			returnObject = grid.get(yCoord).get(xCoord);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return returnObject;
+	}
+
+	/**Modifies object at specific coordinate
+	 * @param xCoord
+	 * @param yCoord
+	 */
+	@SuppressWarnings("unchecked")
+	public void setVal(int xCoord, int yCoord, Object obj) {
+		grid.get(yCoord).set(xCoord, (T) obj);
+	}
+
+	/**Inserts new object at coordinate
+	 * @param xCoord
+	 * @param yCoord
+	 * @param obj
+	 */
+	public void add(int xCoord, int yCoord, Object obj) {
+
+		grid.get(yCoord).add(xCoord, (T) obj);
+		this.xSize = grid.get(yCoord).size();
+		this.ySize = grid.size();
+	}
+
+	@Override
+	/**
+	 * @see java.util.Collection#add(java.lang.Object)
+	 */
+	public boolean add(T e) {
+		try {
+			this.add(this.getxSize() - 1, this.getySize() - 1, e);
+			return true;
+		} catch (Exception e2) {
+			e2.printStackTrace();
+			return false;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Collection#addAll(java.util.Collection)
+	 */
+	public boolean addAll(Collection<? extends T> c) {
+		for (Object item : c) {		
+			this.add((T) item);
+		}
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Collection#remove(java.lang.Object)
+	 */
+	public boolean remove(Object o) {
+		for (int i = 0; i < this.getySize(); i++) {
+			for (int j = 0; j < this.getxSize(); j++) {
+				if (this.getVal(j, i).equals(o)) {
+					this.setVal(j, i, (T) new Object());
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Collection#clear()
+	 */
+	public void clear() {
+		for (int i = 0; i < this.getySize(); i++) {
+			for (int j = 0; j < this.getxSize(); j++) {
+				this.setVal(j, i, new Object());
+			}
+		}
+	}
+
+	/**Returns grid in printable String form
 	 * @return String returnString
 	 */
 	public String getString() {
@@ -65,42 +173,97 @@ public class Grid<T> implements Iterable<T>{
 		return returnString + "]";
 	}
 
-	/**Gets object at a specific coordinate
-	 * @param xCoord
-	 * @param yCoord
-	 * @return returnObject
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Collection#toArray()
 	 */
-	public Object getVal(int xCoord, int yCoord) {
-		Object returnObject = new Object();
-		try {
-			returnObject = grid.get(yCoord).get(xCoord);
-		} catch (Exception e) {
-			e.printStackTrace();
+	public Object[] toArray() {
+		Object[] objArr = new Object[this.size()];
+		int index = 0;
+		for (int i = 0; i < this.getySize(); i++) {
+			for (int j = 0; j < this.getxSize(); j++) {
+				objArr[index] = this.getVal(j, i); 
+			}
 		}
-		return returnObject;
+		return objArr;
 	}
 
-	/**Inserts new object at coordinate
-	 * @param xCoord
-	 * @param yCoord
-	 * @param obj
+	@SuppressWarnings({ "hiding", "unchecked" })
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Collection#toArray(java.lang.Object[])
 	 */
-	public void addVal(int xCoord, int yCoord, Object obj) {
-		grid.get(yCoord).add(xCoord, (T) obj);
-		this.xSize = grid.get(yCoord).size();
-		this.ySize = grid.size();
+	public <T> T[] toArray(T[] a) {
+		T[] tArr = (T[]) new Object[this.size()];
+		int index = 0;
+		for (int i = 0; i < this.getySize(); i++) {
+			for (int j = 0; j < this.getxSize(); j++) {
+				tArr[index] = (T) this.getVal(j, i); 
+			}
+		}
+		return tArr;
 	}
 
-	/**Modifies object at specific coordinate
-	 * @param xCoord
-	 * @param yCoord
+	@SuppressWarnings("unchecked")
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Iterable#iterator()
 	 */
-	public void setVal(int xCoord, int yCoord, Object obj) {
-		grid.get(yCoord).set(xCoord, (T) obj);
+	public Iterator<T> iterator() {
+		return (Iterator<T>) grid.iterator();
 	}
 
 	@Override
-	public Iterator<T> iterator() {
-		return (Iterator<T>) grid.iterator();
+	/*
+	 * @see java.util.Collection#contains(java.lang.Object)
+	 */
+	public boolean contains(Object o) {
+		boolean bool = false;
+		for (int i = 0; i < this.getySize(); i++) {
+			for (int j = 0; j < this.getxSize(); j++) {
+				if (this.getVal(j, i).equals(o)) {
+					bool = true;
+				}
+			}
+		}
+		return bool;
+	}
+
+	@Override
+	/*
+	 * @see java.util.Collection#containsAll(java.util.Collection)
+	 */
+	public boolean containsAll(Collection<?> c) {
+		boolean bool = false;
+
+		return bool;
+	}
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.Collection#isEmpty()
+	 */
+	public boolean isEmpty() {
+		boolean isEmpty = true;
+
+		return isEmpty;
+	}
+
+
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- JavaCode/JavaCode/src/gridDriver.java
+++ JavaCode/JavaCode/src/gridDriver.java
@@ -1,7 +1,8 @@
+
 public class gridDriver {
 
 	public static void main(String[] args) {
-		Grid<Integer> newGrid = new Grid<Integer>(10, 10);
+		Grid<Integer> newGrid = new Grid<Integer>(4, 3);
 		
 		for (int i = 0; i < newGrid.getySize(); i++) {
 			for (int j = 0; j < newGrid.getxSize(); j++) {
