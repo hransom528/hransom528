b50950bca7a90c7b39bd198d2ea8084e32b2d553
Harris Ransom <hransom528@protonmail.com>
2024-06-17 22:28:42 -0400

    Added frequency analysis Python tool

---
+++ Python_Code/freqAnalysis.py
@@ -0,0 +1,88 @@
+# Harris Ransom
+# Cryptographic Frequency Analysis
+# 6/17/2024
+
+# Imports
+import string
+import matplotlib
+import matplotlib.pyplot as plt
+#matplotlib.use('Agg')
+
+# Defines character alphabet
+alphabet = string.ascii_lowercase
+
+# Defines English alphabet frequencies
+englishFrequencies = {
+    'a': 0.08167,
+    'b': 0.01492,
+    'c': 0.02782,
+    'd': 0.04253,
+    'e': 0.12702,
+    'f': 0.02228,
+    'g': 0.02015,
+    'h': 0.06094,
+    'i': 0.06966,
+    'j': 0.00153,
+    'k': 0.00772,
+    'l': 0.04025,
+    'm': 0.02406,
+    'n': 0.06749,
+    'o': 0.07507,
+    'p': 0.01929,
+    'q': 0.00095,
+    'r': 0.05987,
+    's': 0.06327,
+    't': 0.09056,
+    'u': 0.02758,
+    'v': 0.00978,
+    'w': 0.02360,
+    'x': 0.00150,
+    'y': 0.01974,
+    'z': 0.00074
+}
+
+# Frequency Analysis
+def freqAnalysis(text):
+    # Create dictionary to store letter frequencies
+    freqDict = {}
+    total = 0
+    for letter in alphabet:
+        freqDict[letter] = 0
+
+    # Count the frequency of each letter in the text
+    for letter in text:
+        if letter in alphabet:
+            freqDict[letter] += 1
+            total += 1
+
+    # Print the frequency of each letter
+    for letter in freqDict:
+        print(f"{letter}: {freqDict[letter]}")
+
+    # Gets relative frequency of each letter
+    print("\nRelative Frequencies:")
+    frequencies = []
+    for letter in freqDict:
+        letterFrequency = freqDict[letter] / float(total)
+        frequencies.append(letterFrequency)
+        print(f"{letter}: {letterFrequency:.2f}")
+
+    # Plot the relative frequencies
+    fig = plt.figure()
+    plt.bar(range(len(freqDict)), list(freqDict.values()), align='center')
+    plt.xticks(range(len(freqDict)), list(freqDict.keys()))
+    plt.title("Letter Frequencies")
+
+# Main
+def main():
+    # Get user input
+    print("Enter the text you want to analyze: ")
+    text = input().lower()
+    freqAnalysis(text)
+
+# Dunder main
+if __name__ == "__main__":
+    main()
+
+# Test text: 
+#CENERENTOLA.- N bnxm yt ywd nk dtz hfs wjfi ymnx fsi fr rtxy fscntzx yt mjfw ymfy fsi, bmjs dtz wjyzws, fsi mtb qtsl dtzwjrfns, mjwj. It bwnyj f kjb qnsjx ifwqnsl, uqjfxj. N mfaj gjjs ajwd kfw kwtr mfuud xnshj dtz bjsy fbfd.
